73c265bcb207c6742761376e0934889477eef2de
minaripenguin <dhanushvozh345@gmail.com>
2024-08-07 00:56:55 -0700

    innertune music repo added

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo 'Inner tune Repo'
 # packages/apps/innertune music app
-git clone https://github.com/z-huang/InnerTune.wiki.git packages/apps/InnerTune
+git clone https://github.com/HorizonDroidLab/packages_apps_InnerTune packages/apps/InnerTune
 
 echo 'DONE & COMPLETED'
