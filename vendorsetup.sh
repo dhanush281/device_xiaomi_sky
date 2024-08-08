717faef285d8c43a631f832df3224b64741006af
minaripenguin <dhanushvozh345@gmail.com>
2024-08-08 07:44:03 -0700

    repo extrapacks added

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo 'Inner tune Repo'
 # packages/apps/innertune music app
-git clone https://github.com/HorizonDroidLab/packages_apps_InnerTune packages/apps/InnerTune
+git clone https://github.com/AgBKartikey/packages_apps_extrapacks
 
 echo 'DONE & COMPLETED'
