63efece96c643c9f4d2868178b90de6f3c0c1022
minaripenguin <dhanushvozh345@gmail.com>
2024-08-26 02:16:44 -0700

    Revert "Reapply "sky: Speed profile services and wifi-service to reduce RAM and storage""
    This reverts commit 17904f3ddb59909b6d1d86b33017e37d0d1f80dd.

--- device.mk
+++ device.mk
@@ -527,9 +527,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     libsqlite.vendor
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
